f10c54194defa6841e760df521f7cd51241078f1
Nelox <stell4rz@gmail.com>
2026-01-14 09:34:06 +0100

    aze

--- include/js/type.js
+++ include/js/type.js
@@ -37,7 +37,7 @@ let id = urlParams.get('id')
 
 // Remplir la page
 document.querySelector("title").textContent += id
-document.querySelector("h1").textContent = id
+document.querySelector("h1").textContent = "Type " + id
 document.querySelector("h2").textContent = desc_types[types.indexOf(id)]
 
 let pokemons = get_pokemons(data, id)
