82d9c34c6284d117b473ed1a47de1ab8664ff181
Nelox <stell4rz@gmail.com>
2026-01-14 10:15:47 +0100

    Numéro fix Finale

--- include/js/pokemon.js
+++ include/js/pokemon.js
@@ -75,7 +75,7 @@ document.querySelector("#poids").textContent = "Poids : " + pokemon["poids"]
 document.querySelector("#noms").textContent = `Anglais : ${pokemon['nom_en']} ; Japonais : ${pokemon['nom_ja'][1]} (${pokemon['nom_ja'][0]})`
 document.querySelector("#types").innerHTML = format_types(pokemon["type"])
 document.querySelector("#evolutions").innerHTML = format_evolutions(pokemon["evolutions"])
-document.querySelector(".pokemon-number").textContent = `n°${pokemon['id']}`
+document.querySelector(".pokemon-number").textContent = `n°${data.indexOf(pokemon) + 1}` // Affiche le numéro du pokémon dans le titre
 
 // Système de CSS pour chaque page de pokémon
 // Ajoute des classes au <html> pour que le CSS puisse cibler cette page Pokémon
